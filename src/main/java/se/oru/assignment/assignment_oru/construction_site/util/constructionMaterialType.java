d2ceaee04f6893e64545addc76178c1ceedb4691
PaoloForte95 <paolo.forte1995@gmail.com>
2022-05-06 10:04:17 +0300

    Added constructionMaterialType class

---
+++ src/main/java/se/oru/assignment/assignment_oru/construction_site/util/constructionMaterialType.java
@@ -0,0 +1,22 @@
+package se.oru.assignment.assignment_oru.construction_site.util;
+
+
+/**
+ * This class is used to set the Material type
+ * @author paolodell
+ *
+ */
+
+public abstract class constructionMaterialType {
+	
+	/**
+	 * Type of the construction machine
+	 * @author pofe
+	 *
+	 */
+	public static enum MATERIAL_TYPE {SAND, ROCK, SOIL};
+
+}
+
+
+
